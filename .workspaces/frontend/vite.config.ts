10743cf4feda3d1d7ac6f60dec0be07472560ff0
alexy-os <agorbunoff.ru@gmail.com>
2025-04-09 22:19:30 +0300

    Tailwind 4/Vite + Welcome page

--- .workspaces/frontend/vite.config.ts
+++ .workspaces/frontend/vite.config.ts
@@ -1,6 +1,25 @@
-import path from 'path';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
-export default {
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  optimizeDeps: {
+    exclude: ['@tailwindcss/vite']
+  },
+  build: {
+    outDir: '../../public/assets/css',
+    emptyOutDir: false,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/import.css'),
+      output: {
+        assetFileNames: 'tailwind.css'
+      }
+    },
+    minify: false
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -9,4 +28,4 @@ export default {
       '@assets': path.resolve(__dirname, '../../public/assets'),
     },
   },
-};
+});
